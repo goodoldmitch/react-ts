d134948460be66c20b7232b2bd5f58435cf9bfe6
Sergey Kuzin <sergey.kuzin@lemurrr.com>
2025-08-28 10:46:37 +0400

    Form validation added

--- simple-todolist/src/App.tsx
+++ simple-todolist/src/App.tsx
@@ -9,27 +9,45 @@ function App() {
   const [taskTitle, setTaskTitle] = useState('');
   const [taskDescription, setTaskDescription] = useState('');
   const [tasksList, setPostsList] = useState<Task[]>([]);
+  const [formError, setFormError] = useState('');
+
+  function toggleInputError(element : HTMLElement,value : string){
+    if(value.trim()){
+      element.classList.remove('error');
+    }else{
+      element.classList.add('error');
+    }
+  }
 
   function setTitle(e: React.ChangeEvent<HTMLInputElement>) {
-    setTaskTitle(e.target.value);
+    const titleValue = e.target.value;
+    setTaskTitle(titleValue);
+    toggleInputError(e.target, titleValue);
   }
 
   function setDescription(e: React.ChangeEvent<HTMLInputElement>) {
-    setTaskDescription(e.target.value);
+    const descriptionValue = e.target.value
+    setTaskDescription(descriptionValue);
+    toggleInputError(e.target, descriptionValue);
   }
 
   function addTask(e: React.ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const newTask = {
-      id: Date.now(),
-      title: taskTitle,
-      description: '123'
-    }
+    if(taskTitle && taskDescription){
+      const newTask = {
+        id: Date.now(),
+        title: taskTitle,
+        description: taskDescription
+      }
 
-    setPostsList([...tasksList, newTask]);
-    setTaskTitle('');
-    setTaskDescription('');
+      setPostsList([...tasksList, newTask]);
+      setTaskTitle('');
+      setTaskDescription('');
+      setFormError('');
+    }else{
+      setFormError('All fields are required')
+    }
   }
 
   return (
@@ -40,6 +58,7 @@ function App() {
           <input type="text" placeholder="title" onChange={setTitle} value={taskTitle} />
           <input type="text" placeholder="description" onChange={setDescription} value={taskDescription} />
           <button>Add</button>
+          {formError ? <div className='error-block'>All fields are required</div> : ''}
         </form>
         <TasksList tasks={tasksList} />
       </section>
